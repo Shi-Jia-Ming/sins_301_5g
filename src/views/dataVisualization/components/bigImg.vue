6580d6c55317601eb167cd3253350da683106a88
langxixixi <1090659695@qq.com>
2022-03-07 17:25:04 +0800

    关联远程仓库

--- src/views/dataVisualization/components/bigImg.vue
+++ src/views/dataVisualization/components/bigImg.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="box">
-    <img src="https://img1.baidu.com/it/u=1407750889,3441968730&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=799">
+    <img src="@/assets/icon_images/pic_Dbeijingps.png">
   </div>
 </template>
 
